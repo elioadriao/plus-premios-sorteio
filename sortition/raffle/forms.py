84d864283afd126f2f0909b0e0ce71175805e362
Helio Adriao <elio.adriao@gmail.com>
2021-05-20 17:54:19 -0300

    improved quotas buying method

--- sortition/raffle/forms.py
+++ sortition/raffle/forms.py
@@ -9,11 +9,12 @@ class RaffleForm(forms.ModelForm):
         fields = ("title", "description", "quotas", "quota_value", "date", "image")
 
 
-class QuotaForm(forms.ModelForm):
-    class Meta:
-        model = Quota
-        fields = ("number", )
+class QuotaForm(forms.Form):
+    quotas = forms.ModelMultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple, required=True, queryset=Quota.objects.none()
+    )
 
     def __init__(self, *args, **kwargs):
+        raffle_pk = kwargs.pop("raffle_pk", None)
         super().__init__(*args, **kwargs)
-        self.fields["number"].widget = forms.HiddenInput()
+        self.fields["quotas"].queryset = Quota.objects.filter(status="open", raffle_id=raffle_pk)
